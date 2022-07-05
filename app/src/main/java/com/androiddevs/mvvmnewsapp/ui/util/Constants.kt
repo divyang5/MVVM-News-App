77b0282f9d9e47dbcf753f39851fa4c98091b2a8
divyang5 <divyangsumesara4@gmail.com>
2022-07-05 22:25:35 +0530

    First

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object{
+        const val API_KEY="ae33169db10b48e796b2148041efa542"
+        const val BASE_URL="https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY=500L
+    }
+}
